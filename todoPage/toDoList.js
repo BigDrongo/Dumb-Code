79edd4f65520a96ef676c9a23c49fdcff534f5df
BigDrongo <michael.steyn112@gmail.com>
2025-04-15 10:51:29 +0800

    Add files via upload
    a simple to do list
    work in progress...

---
+++ todoPage/toDoList.js
@@ -0,0 +1,16 @@
+
+
+
+let submitBtn = document.getElementById("submitBtn");
+let item = document.getElementById("item"); 
+let inputted = document.getElementById("input");
+
+
+submitBtn = addEventListener("click", function()
+{
+   var paragragh = document.createElement("p")
+   paragraph.innerText = inputted.value;
+   input.appendChild(paragragh);
+
+
+})
